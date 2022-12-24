12659574ef7b12aea72667d4ef93caf4cc64cc42
JetLiTheQT <ekkachai.ittihrit2@gmail.com>
2022-12-23 20:15:06 -0800

    testing calendar

--- src/Calendar.js
+++ src/Calendar.js
@@ -4,7 +4,7 @@ const Calendar = () => {
   return (
     <div>
       <h1>Welcome to My App</h1>
-      <p>This is the home page of my appaaaaaaaaaaaaaa</p>
+      <p>This is the calendar page of my app.</p>
     </div>
   );
 };
